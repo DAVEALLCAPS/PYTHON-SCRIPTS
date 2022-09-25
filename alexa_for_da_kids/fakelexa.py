9258c3b369e11c9a9b2ae9da8f4546d6ff3f1db3
DAVEALLCAPS <daveallcaps@gmail.com>
2022-09-24 22:13:05 -0600

    implement how do you spell and who is

--- alexa_for_da_kids/fakelexa.py
+++ alexa_for_da_kids/fakelexa.py
@@ -1,25 +1,47 @@
 import speech_recognition as sr
 import pyttsx3
+import wikipedia
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
-engine.say('What can I do for you?')
-engine.runAndWait()
+def spellit(text):
+    talk(text)
+    for a in text:
+        talk(a)
+    talk(text)
 
-def sayit(text):
+def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'spell' in command:
-            print(command)
-            sayit(command)
-except:
-    pass
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'how do you spell' in command:
+        command = command.replace('how do you spell', '')
+        print(command)
+        spellit(command)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+
+while True:
+    run_alexa()
